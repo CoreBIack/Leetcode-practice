b4d6f7a1033fb501a94e2134d4aa3172713b3904
Birand Özkara <ozkarabirand@gmail.com>
2025-08-30 17:02:33 +0300

    still not done

--- medium/238.py
+++ medium/238.py
@@ -10,3 +10,14 @@ def solution(nums):
       rtrn.append(prefixs[j] * suffixs[j])
   return rtrn
 print(solution([1, 2, 3, 4]))
+"""
+prefix = nums[0]
+suffix = nums[-1]
+rtrn = [1] * len(nums)
+for i in range(1, len(nums)):
+    rtrn[i] *= prefix 
+    prefix *=  nums[i]
+    rtrn[-i - 1] *= suffix
+    suffix *= nums[-i- 1]
+return rtrn
+"""
